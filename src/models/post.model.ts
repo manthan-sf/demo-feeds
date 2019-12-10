b0e2738b847650dfd6a20566fe58b952e5adf901
Manthan Aneja <manthan.anejaa@sourcefuse.com>
2019-12-10 12:33:48 +0530

    initial commit

---
+++ src/models/post.model.ts
@@ -0,0 +1,32 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Post extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+  })
+  name?: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Post>) {
+    super(data);
+  }
+}
+
+export interface PostRelations {
+  // describe navigational properties here
+}
+
+export type PostWithRelations = Post & PostRelations;
